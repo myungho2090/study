226a3c0e8635ab754c4fd8da6385411d423c1c78
이다휘 <leejgdh@hotmail.com>
2023-12-15 19:42:49 +0900

    예제

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 숨고수업
 hello world
+
+
+if, else , else if, switch
+
+for, while, foreach, map, filter, reduce
--- app.js
+++ app.js
@@ -1,1 +1,198 @@
-console.log("안녕하세요");
+// var a = 5;
+
+// switch (a) {
+//   case 1:
+//     console.log(1);
+//     break;
+//   case 2:
+//     console.log(2);
+//     break;
+//   case 3:
+//     console.log(3);
+//     break;
+//   default:
+//     console.log("default");
+// }
+
+// while(true){
+//     console.log(a);
+
+//     if(reponse){
+//         //resposne = true;
+//         break;
+//     }
+
+//     sleep(1000)
+// }
+
+// for(let i = 0; i< 10; i++){
+//     console.log(i);
+// }
+
+// let c = null;
+// let d = undefined;
+
+// let b = [];
+
+// let a = ["가", "나", "다", "라", "마"];
+
+
+// for (let i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
+
+// e[0][0];
+// e[0][1];
+// e[1][0];
+// e[1][1];
+
+// for (let i = 0; i < e.length; i++) {
+//   for (let o = 0; o < e[i].length; o++) {
+//     console.log(e[(i, o)]);
+//   }
+// }
+
+// for (let i = 0; i < e.length; i++) {}
+
+
+let my_arr = ['a','b','c','d'];
+
+// my_arr.forEach(
+//     (e, i, arr) => {
+//         console.log(e,i,arr);
+//     }
+// );
+
+// my_arr.forEach(
+//     function(e,i,arr){
+//         console.log(e,i,arr);
+//     }
+// )
+
+
+/*
+
+    map *
+    filter *
+    reduce
+
+*/
+
+// let my_arr2 = [1,2,3,4,5];
+
+// let process = my_arr2.map((e,i) =>{
+//     return e + i;
+// })
+
+// console.log('process',process);
+
+// let my_arr3 = [1,2,3,4,5];
+
+// let filter_response = my_arr3.filter((e,i) => {
+
+//     return false;
+
+//     // if(e % 2 == 1){
+//     //     return true;
+//     // }else{
+//     //     return false;
+//     // }
+// })
+
+// console.log('filter_resposne',filter_response);
+
+
+// let my_arr4 = [1,2,3,4,5];
+ 
+// let reduce_response = my_arr4.reduce((p,c,index,arr) => {
+//     return p + c;
+// },0);
+
+// console.log('reduce_response', reduce_response);
+
+
+// let result = 0;
+// for(let i =0; i<my_arr4.length; i++){
+//     result = result + my_arr4[i];
+// }
+
+// let a = 0;
+// a = 1;
+
+// const e = 0;
+// e = 1;
+
+// const buga = 0.1;
+
+
+
+
+// let a = 1;
+
+// console.log(a);
+
+// if(true){
+//     a = 100;
+//     console.log(a);
+// }else{
+//     i = 2;
+//     let a = 0;
+// }
+
+// console.log('a',a);
+
+//!, = , !=, ==, !==, ===
+
+// let a = false;
+// !a = true;
+
+// //1 == 1 = true
+
+// !==
+// ===
+
+// console.log( 1 !== '1');
+
+// //+, ++, +=, -, --, -=, *, /, %
+
+// let a = 1;
+
+// a -= 2  
+
+// a = a - 2;
+
+//이항 연산자
+
+let a = 0;
+
+let b = null ?? 2;
+
+if(a != null){
+    b = a;
+}else{
+    b = 2;
+}
+
+//삼항 연산자
+
+a = 0
+
+let q = a ? 1 : 2;
+
+if(a == 0){
+    q = 1;
+}else{
+    q = 2;
+}
+console.log('q',q);
+
+
+//DATE
+
+
+
+
+
+
+
+
